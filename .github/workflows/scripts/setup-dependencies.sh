3f2c1d7ab8b14aade11f89f96797f3568b6d92ee
zulinfan <fanzulin@ubix.cn>
2025-04-08 16:35:13 +0800

    CI: Modify CI workflow execution
    Remove the codeQL analysis yaml
    Disable run on ubuntu 20.04
    Revert "ZTS: Apply small changes for speeding up the tests"
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- .github/workflows/scripts/setup-dependencies.sh
+++ .github/workflows/scripts/setup-dependencies.sh
@@ -55,24 +55,29 @@ function mod_install() {
   cat /proc/spl/kstat/zfs/chksum_bench
   echo "::endgroup::"
 
-  echo "::group::Optimize storage for ZFS testings"
-  # remove swap and umount fast storage
-  # 89GiB -> rootfs + bootfs with ~80MB/s -> don't care
-  # 64GiB -> /mnt with 420MB/s -> new testing ssd
-  sudo swapoff -a
+  echo "::group::Reclaim and report disk space"
+  # remove 4GiB of images
+  sudo systemd-run docker system prune --force --all --volumes
 
-  # this one is fast and mounted @ /mnt
-  # -> we reformat with ext4 + move it to /var/tmp
-  DEV="/dev/disk/azure/resource-part1"
-  sudo umount /mnt
-  sudo mkfs.ext4 -O ^has_journal -F $DEV
-  sudo mount -o noatime,barrier=0 $DEV /var/tmp
-  sudo chmod 1777 /var/tmp
+  # remove unused software
+  sudo systemd-run --wait rm -rf \
+    "$AGENT_TOOLSDIRECTORY" \
+    /opt/* \
+    /usr/local/* \
+    /usr/share/az* \
+    /usr/share/dotnet \
+    /usr/share/gradle* \
+    /usr/share/miniconda \
+    /usr/share/swift \
+    /var/lib/gems \
+    /var/lib/mysql \
+    /var/lib/snapd
+
+  # trim the cleaned space
+  sudo fstrim /
 
   # disk usage afterwards
-  sudo df -h /
-  sudo df -h /var/tmp
-  sudo fstrim -a
+  df -h /
   echo "::endgroup::"
 }
 
